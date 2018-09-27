369273e66abec4f3d27ef987dd352a94028591d3
w-kaoru <kbc17b28@stu.kawahara.ac.jp>
2018-09-27 11:28:34 +0900

    最初2

--- Game/gameObject/GameObjectManager.h
+++ Game/gameObject/GameObjectManager.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "IGameObject.h"
 
+
 class GameObjectManager
 {
 public:
--- Game/gameObject/IGameObject.h
+++ Game/gameObject/IGameObject.h
@@ -1,15 +1,15 @@
 #pragma once
 class GameObjectManager;
+typedef unsigned char	GameObjectPrio;
 class IGameObject
 {
 public:
 	IGameObject();
 	virtual ~IGameObject();
 	virtual bool Start() { return true; }
-	virtual void Update() {}
-	virtual void Draw() {}
-	friend class CGameObjectManager;
+	virtual void Update() = 0;
+	virtual void Draw() = 0;
 protected:
-	//GameObjectManager * m_gojm = nullptr;
+	GameObjectManager * m_gojm = nullptr;
 };
 
