2d1750ac68e066fed8786aef52fe23933da86d96
w-kaoru <kbc17b28@stu.kawahara.ac.jp>
2019-04-18 10:50:06 +0900

    見える範囲の調整

--- Game/GameCamera.cpp
+++ Game/GameCamera.cpp
@@ -7,7 +7,7 @@ GameCamera::GameCamera()
 	//ƒJƒƒ‰‚ğ‰Šú‰»B
 	g_camera3D.SetPosition({ 0.0f, 200.0f, 370.0f });
 	g_camera3D.SetTarget({ 0.0f, 100.0f, 0.0f });
-	g_camera3D.SetFar(10000.0f);
+	g_camera3D.SetFar(20000.0f);
 }
 
 
