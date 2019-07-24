11b446bb5ac30a9916462e2e79944258b63e9588
w-kaoru <kbc17b28@stu.kawahara.ac.jp>
2019-07-24 12:30:19 +0900

    コメント書きかき

--- Game/Save.h
+++ Game/Save.h
@@ -1,18 +1,23 @@
 #pragma once
 #include "stdafx.h"
 #include "Save.h"
-class Save:public IGameObject
+class Save :public IGameObject
 {
 public:
-	int PlayerLv;
-	float Playerexp;
-	CVector3 position;
+	int PlayerLv; //プレイヤーのレベル保存用。
+	float Playerexp; //プレイヤーの経験値保存用。
 	int SaveStage;
 	//CVector3 position;
-	void NeworLoadGame(int l, float e, int s = 0)
+	/*!
+		* @brief	初期化。
+		*@param[in]	Lv		レベルの初期化。
+		*@param[in]	EXP		経験値の初期化。
+		*@param[in]	Stage	ステージの初期化。
+	*/
+	void NeworLoadGame(int Lv, float EXP, int Stage = 0)
 	{
-		PlayerLv = l;
-		Playerexp = e;
-		SaveStage = s;
+		PlayerLv = Lv;
+		Playerexp = EXP;
+		SaveStage = Stage;
 	}
 };
