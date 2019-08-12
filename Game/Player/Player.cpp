88c98428e665b9cc55e6132531ff354c5556c529
w-kaoru <kbc17b28@stu.kawahara.ac.jp>
2019-08-12 13:09:36 +0900

    調整

--- Game/Player/Player.cpp
+++ Game/Player/Player.cpp
@@ -59,7 +59,7 @@ Player::Player() :m_stMa(this)
 	//m_status.SetStandardAgi(1850.0f);
 	m_status.SetStandardDef(1.0f);
 	m_status.SetStandardAtk(13.5f);
-	m_status.SetStandardAtk(1300.5f);
+	//m_status.SetStandardAtk(1300.5f);
 	m_status.SetMaxLv(30);
 }
 Player::~Player()
